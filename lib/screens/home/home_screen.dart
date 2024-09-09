bd8f1b6699e01cb859835366a36d479240b3cf0d
asmaamohamer123 <asmaaseoudy112@gmail.com>
2024-09-09 21:26:27 +0300

    w

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -7,8 +7,9 @@ class HomeScreens extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    
     final List<Website> _websites = [
-      //webs and apps
+      //"webs and apps ""
       Website('شئون الطلاب', 'https://fcaisys.fayoum.edu.eg', "assets/images/students.jfif"),
       Website('الارشيف الالكتروني', 'https://www.google.com', "assets/images/archive.jpg"),
       Website('خدمات الدراسات العليا', 'https://fcipost.fayoum.edu.eg', "assets/images/master.jfif"),
